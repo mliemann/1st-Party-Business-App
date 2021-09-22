81356d5fdea1638f42afccc7b1edf24d31855909
Dauphine Burns <dauphineburns@gmail.com>
2021-09-22 12:45:44 -0400

    adding styling to landing page

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -8,13 +8,13 @@ function Landing () {
                 <div className="cover">
                     <div id="background1">
                         <img id="coverimg1" src="https://res.cloudinary.com/dl0hsgmfc/image/upload/v1631985686/tacotavern/taco-cover-1.jpg" alt=""></img>
-                        <h1 id="viewmenu">VIEW MENU</h1>
+                        <h1 id="viewmenu">view menu</h1>
                     </div>
                 </div>
                 <div className="cover">
                     <div id="background2">
-                        <h1 id="placeorder">PLACE YOUR ORDER</h1>
-                        <img id="coverimg2" src="https://res.cloudinary.com/dl0hsgmfc/image/upload/v1631985686/tacotavern/taco-cover-5.jpg" alt=""></img>
+                        <h1 id="placeorder">place your order</h1>
+                        <img id="coverimg2" src="https://res.cloudinary.com/dl0hsgmfc/image/upload/v1631985686/tacotavern/taco-cover-16.jpg" alt=""></img>
                     </div>
                 </div>
                 
