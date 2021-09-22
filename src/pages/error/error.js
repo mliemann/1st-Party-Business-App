8c2a244a54df4afd97a11121567ab1120ebbe8b3
Dauphine Burns <dauphineburns@gmail.com>
2021-09-22 15:17:34 -0400

    404 page made and styled

---
+++ src/pages/error/error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./error.css";
+
+
+function Error () {
+    return (
+       <div>
+           <h1 id="error">404!</h1>
+       </div>
+    )
+}
+
+export default Error;
