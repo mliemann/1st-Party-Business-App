8c6bded3f2233d7504ee0525866b4459f7bd39ba
Marlon Liemann <awesomemarlon@gmail.com>
2021-09-22 20:45:49 -0400

    initial cart styling

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -3,17 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./cart.css";
 
 function Cart(props) {
+  
   return (
-    <table className="table sortable">
-      <thead>
-        <tr>
-          <th scope="col">Product</th>
-          <th scope="col">Quantity</th>
-          <th scope="col">Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.users.map((user) => {
+    <div id="cart1">
+      <table className="table sortable" >
+        <thead >
+          <tr>
+            <th scope="col">Product</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {props.users.map((user) => {
           return (
             <tr key={user.id}>
               <td data-label="Product">{user.product}</td>
@@ -21,23 +23,34 @@ function Cart(props) {
               <td data-label="Price">{user.price}</td>
             </tr>
           );
-        })}
-        <thead>
+        })} */}
+          <tr>
+            <td data-label="Product">Product1</td>
+            <td data-label="Quantity">Product1</td>
+            <td data-label="Price">Product1</td>
+          </tr>
+        </tbody>
+      </table>
+      <div id= "pricetable"> 
+      <table className="table sortable" >
+        <tbody >
           <tr>
             <th data-field="subtotal">Subtotal</th>
-            <th data-field="tax">Tax</th>
-            <th data-field="total">Total</th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
             <td>Subtotal</td>
+          </tr>
+
+          <tr>
+            <th data-field="tax">Tax</th>
             <td>Tax</td>
+          </tr>
+          <tr>
+            <th data-field="total">Total</th>
             <td>Total</td>
           </tr>
-        </tfoot>
-      </tbody>
-    </table>
+        </tbody>
+        </table>
+        </div>
+    </div>
   );
 }
 
