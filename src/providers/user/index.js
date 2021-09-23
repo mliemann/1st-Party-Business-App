dd9212f5ddd667af48dab1e2d0388e2506b89fb9
Mike Drew <drewml08@gmail.com>
2021-09-23 17:19:23 -0400

    moved user data into provider component, created providers directory to hold provider components, added logout button to header component, login/logout buttons shown or hidden depending on logged in state

---
+++ src/providers/user/index.js
@@ -0,0 +1,49 @@
+import React, { createContext, useReducer, useContext } from "react";
+import {
+  USER_LOGGED_IN,
+  USER_LOGGED_OUT
+} from "./actions";
+
+const UserContext = createContext();
+const { Provider } = UserContext;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case USER_LOGGED_IN:
+    return {
+      ...state,
+      userData: action.userData,
+      isLoggedIn: true
+    };
+
+  case USER_LOGGED_OUT:
+    return {
+      ...state,
+      userData: null,
+      isLoggedIn: false
+    };
+    
+  default:
+    return state;
+  }
+};
+
+const UserProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    userData: null,
+    isLoggedIn: false
+  });
+
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useUserContext = () => {
+  return useContext(UserContext);
+};
+
+export { 
+    UserProvider, 
+    useUserContext,
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT
+ };
