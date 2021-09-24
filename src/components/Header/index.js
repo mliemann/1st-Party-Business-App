5f4f4bb3d55826462d80f71a03e0c0b42f84c70e
Marlon Liemann <awesomemarlon@gmail.com>
2021-09-23 20:18:45 -0400

    css fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,9 +24,9 @@ function Header() {
   // const pageRoute6 = () => {
   //   history.push("/menu");
   // };
-  // const pageRoute7 = () => {
-  //   history.push("/profile");
-  // };
+  const pageRoute7 = () => {
+    history.push("/profile");
+  };
   // const pageRoute8 = () => {
   //   history.push("/restsignup");
   // };
@@ -44,7 +44,6 @@ function Header() {
 
   return (
     <div className="header">
-      
         <img id="logo"
             src="https://res.cloudinary.com/dl0hsgmfc/image/fetch/e_replace_color:0e1b47/https://bestteamproj2.s3.amazonaws.com/TacoTavern.png"
             alt="taco tavern logo"
@@ -54,16 +53,30 @@ function Header() {
         <div id="whitespace">
         
         { state.isLoggedIn ? 
-          <button className="btn" id="logoutbtn" onClick={logUserOut}>logout</button> : 
-          <button className="btn" id="loginbtn" onClick={pageRoute1}>login</button> }
+        <div>  <button className="btn" id="logoutbtn" onClick={logUserOut}>logout</button><button className="btn" id="cartbtn"  onClick={pageRoute2}>cart</button>
+    {/* <button onClick={pageRoute3}>Home</button> */}
+    <button className="btn" id="landingbtn" onClick={pageRoute4}>home</button>
+    {/* <button onClick={pageRoute5}>Dashboard</button> */}
+    {/* <button onClick={pageRoute6}>Menu</button> */}
+    <button className="btn" id="landingbtn" onClick={pageRoute7}>profile</button>
+    {/* <button onClick={pageRoute8}>Restaurant Signup</button> */}
+    <img id="profilepic"
+        src=" https://res.cloudinary.com/dl0hsgmfc/image/fetch/c_crop,g_face,h_200,w_200/c_scale,w_200/https://bestteamproj2.s3.amazonaws.com/images/Pictureofme.jpg"
+        alt="taco tavern logo"
+        width="auto"
+        height="175"
+    ></img> </div>
+          : 
+          <div><button className="btn" id="loginbtn" onClick={pageRoute1}>login</button>  <button className="btn" id="cartbtn"  onClick={pageRoute2}>cart</button>
+          {/* <button onClick={pageRoute3}>Home</button> */}
+          <button className="btn" id="landingbtn" onClick={pageRoute4}>home</button>
+          {/* <button onClick={pageRoute5}>Dashboard</button> */}
+          {/* <button onClick={pageRoute6}>Menu</button> */}
+          <button className="btn" id="landingbtn" onClick={pageRoute7}>profile</button>
+          {/* <button onClick={pageRoute8}>Restaurant Signup</button> */}</div>
+          }
         
-        <button className="btn" id="cartbtn"  onClick={pageRoute2}>Cart</button>
-        {/* <button onClick={pageRoute3}>Home</button> */}
-        <button className="btn" id="landingbtn" onClick={pageRoute4}>home</button>
-        {/* <button onClick={pageRoute5}>Dashboard</button> */}
-        {/* <button onClick={pageRoute6}>Menu</button> */}
-        {/* <button onClick={pageRoute7}>Profile</button> */}
-        {/* <button onClick={pageRoute8}>Restaurant Signup</button> */}
+       
         </div>
     </div>
   );
