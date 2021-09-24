d864d3490dacf6808299b0904074540dd09259bf
Tyler Davis <tdnc85@yahoo.com>
2021-09-23 22:14:06 -0400

    updated file structure and package.json files to run server and react with one npm start command

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const tableRoutes = require("./tableRoutes");
+// const tableRoutes = require("./tableRoutes");
 
 
 router.use("/user", userRoutes);
-router.use("/tables", tableRoutes);
+// router.use("/tables", tableRoutes);
 
 
 module.exports = router;
