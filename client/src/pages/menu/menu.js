0a14879327db704ac009e7cbc47c2179ea0ddd6f
Tyler Davis <tdnc85@yahoo.com>
2021-09-25 21:52:48 -0400

    added info hover

--- client/src/pages/menu/menu.js
+++ client/src/pages/menu/menu.js
@@ -1,39 +1,68 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 import "./menu.css";
 import products from "../../menu.json";
+// import Dropdown from "react-bootstrap"
+// import Details from "../../components/Details/details"
 
-function Menu (props) {
-    return (
-        // <div className="container">
-        //     <div id="menu">
-        //         <div className="menuItem">
-        //             <div id={"dish" + props.id} >{props.dish}</div>
-        //             <div id={"description" + props.id}>{props.description}</div>
-        //             <div id={"price" + props.id}>{props.price}</div>
-        //             <button id="moreInfoBtn">More details</button>
-        //             <button id="addCartBtn">Add to card</button>
-        //         </div>
-        //     </div>
-        // </div>
-       
+function Menu() {
+  return (
+    // <div className="container">
+    //     <div id="menu">
+    //         <div className="menuItem">
+    //             <div id={"dish" + props.id} >{props.dish}</div>
+    //             <div id={"description" + props.id}>{props.description}</div>
+    //             <div id={"price" + props.id}>{props.price}</div>
+    //             <button id="moreInfoBtn">More details</button>
+    //             <button id="addCartBtn">Add to card</button>
+    //         </div>
+    //     </div>
+    // </div>
 
-        <div className="containermenu">
-        {products.map((product) => {
-            return (
-        <div key={product.id} id="menu">
+    <div className="containermenu">
+      <h1 id="menu-header">platos</h1>
+      {products.map((product) => {
+        return (
+          <div key={product.id} id="menu">
             <div className="menuItem">
-            <h1>platos</h1>
-                <div className="item" id="dish" >{product.dish}</div>
-                <div className="item" id="description">{product.description}</div>
-                <div className="item" id="price">{product.price}</div>
-                <button className="item" id="moreInfoBtn">More details</button>
-                <button className="item" id="addCartBtn">Add to card</button>
+              <div className="item" id="dish">
+                {product.dish}
+                <a
+                  className="ui-tooltip"
+                  title={product.description}
+                >
+                  <span style={{ cursor: "pointer" }}>
+                    <i className="fas fa-info-circle"></i>
+                  </span>
+                </a>
+              </div>
+              <div className="item" id="description">
+                {product.description}
+              </div>
+              <div className="item" id="price">
+                {product.price}
+              </div>
+              {/* <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    details
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item>{product.description}</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown> */}
+
+              {/* <button className="item" id="moreInfoBtn">More details</button> */}
+
+              <button className="item" id="addCartBtn">
+                Add to card
+              </button>
             </div>
-        </div>
-            )
-        })}
+          </div>
+        );
+      })}
     </div>
-    )
+  );
 }
 
 export default Menu;
