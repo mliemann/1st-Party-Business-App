d1b0077271b05f88e27df7ff6eb7a37485ed0ddc
Tyler Davis <tdnc85@yahoo.com>
2021-09-28 22:03:30 -0400

    minor changes and formatting files

--- client/src/pages/contact/contact.js
+++ client/src/pages/contact/contact.js
@@ -2,35 +2,61 @@ import React from "react";
 import "./contact.css";
 
 function Contact() {
+  return (
+    <div className="containercontact">
+      <form id="formcontact" action="action_page.php">
+        <div>
+          <h2 id="contact">contact us:</h2>
+        </div>
 
-    return (
-        <div className="containercontact" >
-          
-  <form id="formcontact" action="action_page.php">
-    <div>
-      <h2 id="contact">contact us:</h2>
+        <label className="title" htmlFor="fname">
+          first name
+        </label>
+        <input
+          className="input"
+          type="text"
+          id="fname"
+          name="firstname"
+          placeholder="    your name..."
+        ></input>
+
+        <label className="title" htmlFor="lname">
+          last name
+        </label>
+        <input
+          className="input"
+          type="text"
+          id="lname"
+          name="lastname"
+          placeholder="    your last name..."
+        ></input>
+
+        <label className="title" htmlFor="lname">
+          email
+        </label>
+        <input
+          className="input"
+          type="text"
+          id="lname"
+          name="lastname"
+          placeholder="    your email..."
+        ></input>
+
+        <label className="title" htmlFor="subject">
+          subject
+        </label>
+        <textarea
+          className="input"
+          id="subject"
+          name="subject"
+          placeholder="               write something..."
+          style={{ height: "200px" }}
+        ></textarea>
+
+        <input className="btnlogin" type="submit" value="submit"></input>
+      </form>
     </div>
-
-    <label className="title" htmlFor="fname">first name</label>
-    <input className="input" type="text" id="fname" name="firstname" placeholder="    your name..."></input>
-
-    <label className="title" htmlFor="lname">last name</label>
-    <input className="input" type="text" id="lname" name="lastname" placeholder="    your last name..."></input>
-
-    <label className="title" htmlFor="lname">email</label>
-    <input className="input" type="text" id="lname" name="lastname" placeholder="    your email..."></input>
-
-    <label className="title" htmlFor="subject">subject</label>
-    <textarea className="input" id="subject" name="subject" placeholder="               write something..." style={{height: "200px"}}></textarea>
-
-    <input className="btnlogin" type="submit" value="submit"></input>
-
-  </form>
-</div>
-    )
-
- 
-
+  );
 }
 
 export default Contact;
