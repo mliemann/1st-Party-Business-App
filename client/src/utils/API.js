5af399574e9d23ddb9f5e94f54c753ba7cb9bcc4
Dauphine Burns <dauphineburns@gmail.com>
2021-09-30 21:28:02 -0400

    cleaned up code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const backend = axios.create({
-    baseURL: "/"
+    baseURL: "http://localhost:3001"
 });
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
