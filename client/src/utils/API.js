306d76bed03a6627c4364f5c1bb40fd637f59d8c
Mike Drew <drewml08@gmail.com>
2021-10-05 17:15:31 -0400

    changed user id from being URL parameter and pushed into request body instead to fix routing issue with /api/dish/like handler.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,15 +5,16 @@ const backend = axios.create({
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getDishes: function(user_id) {
-        return backend.get("/api/dish/" + user_id);
+        return backend.get("/api/dish/", {user_id: user_id});
     },
     loginUser: function(credentials) {
         return backend.post("/api/user/login", credentials);
     },
-    doLike: function(dish_id, user_id, action) {
-        return backend.post("/api/dish/" + dish_id + "/like/" + user_id, action)
+    doLike: function(dish_id, action) {
+        return backend.post("/api/dish/" + dish_id + "/like/", action)
     },
     getLike: function() {
-        return backend.get("/api/dish/like")
+    //    return backend.get("/api/dish/like")
+        return backend.get("/api/dish/", {user_id: 0})
     }
 }
